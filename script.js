03f02a4834be42817e9d7d901245b2859e67b9f3
EmanueleAnnoscia <annosciaemanuele@gmail.com>
2025-05-05 12:38:17 +0200

    feat somma e media

---
+++ script.js
@@ -0,0 +1,28 @@
+const mele = [
+    { varieta: "Varietà 1", peso: 10, diametro: 4 },
+    { varieta: "Varietà 2", peso: 13, diametro: 16 },
+    { varieta: "Varietà 3", peso: 4, diametro: 23 },
+    { varieta: "Varietà 4", peso: 11, diametro: 6 },
+    { varieta: "Varietà 5", peso: 2, diametro: 17 },
+    { varieta: "Varietà 6", peso: 5, diametro: 10 },
+    { varieta: "Varietà 7", peso: 7, diametro: 9 },
+    { varieta: "Varietà 8", peso: 3, diametro: 8 },
+    { varieta: "Varietà 9", peso: 6, diametro: 27 },
+    { varieta: "Varietà 10", peso: 14, diametro: 4 },
+  ];
+  
+  let pesoTotale=0;
+
+  for (i=0; i<mele.length; i++){
+      pesoTotale += mele[i].peso; 
+      
+  }
+
+//   media
+
+let media = pesoTotale/mele.length
+
+
+
+console.log(pesoTotale);
+console.log(media)
